027994147ed20063680fbf41ebfba5e61c233ede
차준호 <ckwnsgh4@naver.com>
2026-03-24 13:02:54 +0900

    init

---
+++ src/main/java/org/myproject/inhastudy/InhaStudyApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.inhastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InhaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myproject/inhastudy/InhaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.inhastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
